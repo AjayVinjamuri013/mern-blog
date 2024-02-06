5e9f2004643fad088b56451c750051c5980be324
Ajay <420randykumar@gmail.com>
2024-02-06 13:11:29 -0600

    Completed the BAse UI. Now onto routing.

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,54 @@ import './App.css';
 
 function App() {
   return (
-    <div>testttt
-    </div>
+    <main>
+      <header>
+        <a href="" className='logo'>MyBlog</a>
+        <nav>
+          <a href=''>Login</a>
+          <a href=''>Register</a>
+        </nav>
+      </header>
+      <div className='post'>
+        <div className="image">
+          <img src="https://www.w3.org/TR/css3-flexbox/images/flex-pack.svg" alt=""></img>
+        </div>
+        <div className="texts">
+          <h2>Title</h2>
+          <p className="info">
+            <a className="author">Ajay</a>
+            <time>2023-01-01 10:40</time>
+          </p>
+          <p className="summary">In this tutorial, I'm going to show you how to build a fullstack blog app </p>
+        </div> 
+      </div>
+      <div className='post'>
+        <div className="image">
+          <img src="https://www.w3.org/TR/css3-flexbox/images/flex-pack.svg" alt=""></img>
+        </div>
+        <div className="texts">
+          <h2>Title</h2>
+          <p className="info">
+            <a className="author">Ajay</a>
+            <time>2023-01-01 10:40</time>
+          </p>
+          <p className="summary">In this tutorial, I'm going to show you how to build a fullstack blog app </p>
+        </div> 
+      </div>
+      <div className='post'>
+        <div className="image">
+          <img src="https://www.w3.org/TR/css3-flexbox/images/flex-pack.svg" alt=""></img>
+        </div>
+        <div className="texts">
+          <h2>Title</h2>
+          <p className="info">
+            <a className="author">Ajay</a>
+            <time>2023-01-01 10:40</time>
+          </p>
+          <p className="summary">In this tutorial, I'm going to show you how to build a fullstack blog app </p>
+        </div>  
+      </div>
+    </main>
   );
 }
 
